60b1ca735672addcaed3da4bddfe5c4fd54074cb
Mahmoud alrashidi <147242051+ALRASHIDI-D@users.noreply.github.com>
2024-01-22 09:32:08 +0300

    connect colors

--- lib/core/utils/consts.dart
+++ lib/core/utils/consts.dart
@@ -1,6 +1,7 @@
 import 'package:common_setup/core/utils/entities/customize_app_entity.dart';
 import 'package:common_setup/dependency_injection.dart'
     as setup_package_injection;
+import 'package:flutter/material.dart';
 
 class AppConstants {
   static String tokenKeyForLocalStorage = "token_key";
@@ -15,6 +16,10 @@ class AppConstants {
   static String arabicFontFamily = 'GE_Flow_Regular';
 
   static String englishFontFamily = 'Lato';
+  static Color primaryColor =
+      setup_package_injection.sl<CustomizeAppEntity>().primaryColor;
+  static Color secondColor =
+      setup_package_injection.sl<CustomizeAppEntity>().secondColor;
 }
 
 enum TokenTypes { bearer, basic }
